d283193a3209ed773f72d0f9cca6c02c38ac610d
abhishek sukumar <abhishekskm44@gmail.com>
2022-04-24 12:09:13 +0530

    commit

---
+++ src/Card/Components/Device.jsx
@@ -0,0 +1,6 @@
+
+
+const Device = ({title}) => {
+    return <h5>{title}</h5>
+}
+export default Device;
---
+++ src/Card/Components/Sub.jsx
@@ -0,0 +1,7 @@
+
+const Sub = ({title}) => {
+    return <div>
+        <h1>{title}</h1>
+    </div>
+}
+export default Sub;
